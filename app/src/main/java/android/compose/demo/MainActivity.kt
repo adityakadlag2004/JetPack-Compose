873c3d6a187fc5d47722bdc02a36c263c53677a6
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-12-26 13:35:40 +0530

    Chahush ahsu idpsifh sd hfudhsfpu

--- app/src/main/java/android/compose/demo/MainActivity.kt
+++ app/src/main/java/android/compose/demo/MainActivity.kt
@@ -1,20 +1,8 @@
 package android.compose.demo
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +10,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.fillMaxSize()) {
-                val color = remember { mutableStateOf(Color.Yellow) }
-                ColorBox(
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxSize()
-                ){
-                    color.value=it
-                }
-                Box(modifier = Modifier
-                    .background(color.value)
-                    .weight(1f)
-                    .fillMaxSize()) {
 
-                }
-            }
         }
     }
 
@@ -45,21 +18,3 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@SuppressLint("UnrememberedMutableState")
-@Composable
-fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit) {
-    Box(modifier = modifier
-        .background(Color.Red)
-        .clickable {
-            updateColor(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            )
-        }) {
-
-    }
-}
