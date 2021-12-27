d7b0bacbf67aedd9673b41955703380b573f3b0f
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-12-27 09:29:49 +0530

    Lists - Android Jetpack Compose - Part 8

--- app/src/main/java/android/compose/demo/MainActivity.kt
+++ app/src/main/java/android/compose/demo/MainActivity.kt
@@ -3,39 +3,36 @@ package android.compose.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val modifier =
-                Modifier
-                    .padding(10.dp)
-                    .background(Color.Green)
-                    .fillMaxWidth(1f)
-                    .fillMaxHeight(0.5f)
-            Column(modifier=modifier,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Greeting(name = "Hello")
-                Greeting(name = "Aditya")
+            LazyColumn() {
+                items(1000) {
+                    Text(
+                        text = "text $it",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )
+
+                }
             }
         }
-    }
-
-    @Composable
-    fun Greeting(name: String) {
-        Text(text = name)
     }
 
 
