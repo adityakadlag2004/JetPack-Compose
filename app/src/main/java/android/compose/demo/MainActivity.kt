c452f45c837a977d88b4c1b771cf0360bcffb569
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-01-02 15:54:11 +0530

    ConstraintLayout - Android Jetpack Compose - Part 9

--- app/src/main/java/android/compose/demo/MainActivity.kt
+++ app/src/main/java/android/compose/demo/MainActivity.kt
@@ -3,35 +3,61 @@ package android.compose.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn() {
-                items(1000) {
-                    Text(
-                        text = "text $it",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("greenBox")
 
+                val redBox = createRefFor("redBox")
+
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(200.dp)
+                    height = Dimension.value(200.dp)
                 }
+
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.value(200.dp)
+                    height = Dimension.value(200.dp)
+                }
+
             }
+
+
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+
+
+                    Box(
+                        modifier = Modifier
+                            .background(Color.Green)
+                            .layoutId("greenBox")
+                    )
+                    Box(
+                        modifier = Modifier
+                            .background(Color.Red)
+                            .layoutId("redBox")
+                    )
+                }
+
         }
     }
 
