0e289103c7696249f9318b40abd9f3821f5512af
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-01-12 12:00:48 +0530

    Animations Progress Bar

--- app/src/main/java/android/compose/demo/MainActivity.kt
+++ app/src/main/java/android/compose/demo/MainActivity.kt
@@ -3,41 +3,77 @@ package android.compose.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val modifier =
-                Modifier
-                    .padding(10.dp)
-                    .background(Color.Green)
-                    .fillMaxWidth(1f)
-                    .fillMaxHeight(0.5f)
-            Column(modifier=modifier,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Greeting(name = "Hello")
-                Greeting(name = "Aditya")
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()){
+                CircularProgressBar(percentage = 1f, number =100 )
             }
         }
     }
 
     @Composable
-    fun Greeting(name: String) {
-        Text(text = name)
-    }
+    fun CircularProgressBar(
+        percentage: Float,
+        number: Int,
+        fontSize: TextUnit = 23.sp,
+        radius: Dp = 50.dp,
+        color: Color = Color.Green,
+        strokeWidth: Dp = 8.dp,
+        animDuration: Int = 1000,
+        animDelay: Int = 0
+    ) {
+        var animationPlayed by remember {
+            mutableStateOf(false)
+        }
+        val curPercentage =
+            animateFloatAsState(
+                targetValue = if (animationPlayed) percentage else 0f,
+                animationSpec = tween(durationMillis = animDuration, delayMillis = animDelay)
+            )
+        LaunchedEffect(key1 = true) {
+            animationPlayed = true
+        }
 
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.size(radius * 2f)) {
+            Canvas(modifier = Modifier.size(radius * 2f)) {
+                drawArc(
+                    color = color,
+                    startAngle = -90f,
+                    360 * curPercentage.value,
+                    useCenter = false,
+                    style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+                )
+            }
+            Text(
+                text = (curPercentage.value * number).toInt().toString(),
+                color = Color.Black,
+                fontSize = fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 
 }
 
