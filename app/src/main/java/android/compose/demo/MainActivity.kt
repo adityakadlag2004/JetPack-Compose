75b9adb0184b769cfe3f67e85d9a825db510f426
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-01-29 14:33:10 +0530

    Compose navigation

--- app/src/main/java/android/compose/demo/MainActivity.kt
+++ app/src/main/java/android/compose/demo/MainActivity.kt
@@ -3,41 +3,24 @@ package android.compose.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val modifier =
-                Modifier
-                    .padding(10.dp)
-                    .background(Color.Green)
-                    .fillMaxWidth(1f)
-                    .fillMaxHeight(0.5f)
-            Column(modifier=modifier,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Greeting(name = "Hello")
-                Greeting(name = "Aditya")
-            }
+            Navigation()
         }
     }
 
-    @Composable
-    fun Greeting(name: String) {
-        Text(text = name)
-    }
 
+    @Preview
+    @Composable
+    fun ComposablePreview() {
+        Navigation()
+    }
 
 }
 
